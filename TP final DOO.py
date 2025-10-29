83d179c940d63056b608ff84dc811e3d65290189
Daniela Comes <daniela.comes@outlook.com>
2025-10-29 12:32:50 -0300

    Hola

--- TP final DOO.py
+++ TP final DOO.py
@@ -2,114 +2,51 @@ import sqlite3
 conexion= None
 
 try:
-    conexion= sqlite3.connect("Base de datos pax.db")
+    conexion= sqlite3.connect("Base de datos pax.sql")
     cursor= conexion.cursor()
-    cursor.execute("Select SQLite_version()")
+    cursor.execute("Select SQLITE_VERSION()")
     datos= cursor.fetchone()
+    print("SQLite esta en version: %s" % datos)
+
 except sqlite3.Error as e:
-    print("Error %s: " %e.args [0])
+    print("Error %s:" %e.args [0])
+
 finally:
     if conexion:
         conexion.close()
 
 class Pasajeros:
     def __init__(self):
-        pass
-
-    def __del__(self):
-        pass
-
-    def agregar(self):
         self.nombre= input("Ingrese nombre y apellido del pasajero: ")
         self.DNI= int(input("Ingrese DNI del pasajero: "))
         self.fecha_nac= input("Ingrese fecha de nacimiento del pasajero: ")
         self.telefono= int(input ("Ingrese número de telefono del pasajero: "))
         self.mail= input("Ingrese mail del pasajero: ")        
-        self.paquete= input("Seleccione el paquete que le guste: ")        
-        print(self.nombre)
-        print("{:,}".format(self.DNI))
-        print(self.fecha_nac)
-        print(self.telefono)
-        print(self.mail)
-        print(self.paquete)
-        print("Pasajero agregado.")
-        print("1. Sí")
-        print("0. No")
-        opcion= int(input("¿Desea agregar otro pasajero? "))
-        if opcion==1:
-            self.agregar()
-        elif opcion==0:
-            self.menu()
-        else:
-            print("Opción incorrecta.")
-
-    def eliminar(self):
-        self.__del__()
-        print("Producto eliminado.")
-        print("1. Sí")
-        print("0. No")
-        opcion= int(input("¿Desea eliminar otro pasajero? "))
-        if opcion==1:
-            self.eliminar()
-        elif opcion==0:
-            self.menu()
-        else:
-            print("Opción incorrecta.")
-
-    def modificar(self):
-        self.nombre= input("Ingrese nombre y apellido del pasajero: ")
-        self.DNI= int(input("Ingrese DNI del pasajero: "))
-        self.fecha_nac= input("Ingrese fecha de nacimiento del pasajero: ")
-        self.telefono= int(input ("Ingrese número de telefono del pasajero: "))
-        self.mail= input("Ingrese mail del pasajero: ")        
-        self.paquete= input("Seleccione el paquete que le guste: ")
-        print(self.nombre)
-        print("{:,}".format(self.DNI))
-        print(self.fecha_nac)
-        print(self.telefono)
-        print(self.mail)
-        print(self.paquete)
-
-    def salir(self):
-        print("¡Gracias!")
-
-    def menu(self):
-        print("1. Agregar pasajero")
-        print("2. Eliminar pasajero")
-        print("3. Modificar datos del pasajero")
-        print("4. Salir")
-        seleccion= int(input("Ingrese una opción: "))
-        while True:
-            if seleccion==1:
-                self.agregar()
-                break
-            elif seleccion==2:
-                self.eliminar()
-                break
-            elif seleccion==3:
-                self.modificar()
-                break
-            elif seleccion==4:
-                self.salir()
-                break
-            else:
-                print("Opción inválida.")
-                break
-        self.menu()    
+        self.paquete= input("Seleccione el paquete que le guste: ")                
 
 pasajero= Pasajeros()
-pasajero.menu()
-pasajero.agregar()
-pasajero.eliminar()
-pasajero.modificar()
-pasajero.salir()
 
-conexion= sqlite3.connect("Base de datos pax.db")
+conexion= sqlite3.connect("Base de datos pax.sql")
 with conexion:
     cursor= conexion.cursor()
-Lista_pasajeros= []
+
+#Ejecución del script SQL:
 id_pasajeros= 0
-for pasajeros in Lista_pasajeros:
-    cursor.execute("Agregar en Pasajeros", (id_pasajeros, Pasajeros.nombre, Pasajeros.DNI, Pasajeros. fecha_nac, Pasajeros.telefono,
-    Pasajeros.mail, Pasajeros.paquete))
-id_pasajeros+=1
+for pasajeros in Pasajeros:
+    cursor.execute("INSERT INTO Pasajeros VALUES()", (id_pasajeros, pasajeros.nombre, pasajeros.DNI, pasajeros. fecha_nac, pasajeros.telefono,
+    pasajeros.mail, pasajeros.paquete))
+id_pasajeros+= 1
+
+#Recuperar los registros:
+cursor.execute ("SELECT * FROM Base de datos pax")
+lineas= cursor.fetchall()
+for linea in lineas:
+    print (linea)
+
+#Modificar un campo:
+cursor.execute("UPDATE Pasajeros SET mail WHERE nombre")
+conexion. commit()
+print("Numero de lineas actualizadas: %d" % cursor.rowcount)
+
+#Eliminar un registro:
+cursor.execute("DELETE FROM Pasajeros WHERE ID= 2")
