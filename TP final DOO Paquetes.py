ea6242907b8722c9266d68a869d0d7e15859ebe6
Daniela <daniela.comes@outlook.com>
2025-11-06 10:54:04 -0300

    Cambios 3

--- TP final DOO Paquetes.py
+++ TP final DOO Paquetes.py
@@ -14,24 +14,46 @@ try:
     print("Tabla 'paquetes' verificada/creada.")
 
 #3. Insertar datos
-    Lista_paquetes= [
-    ('Ushuaia + Calafate', 'Incluye aéreos, alojamiento, traslados y excursiones', "$ {:,}".format(1000000)),
-    ('Salta', 'Incluye aéreos, alojamiento, traslados y excursiones', "$ {:,}".format(800000)),
-    ('Mendoza', 'Incluye aéreos, alojamiento, traslados y excursiones', "$ {:,}".format(600000)),
-    ('Rio de Janeiro', 'Incluye aéreos y alojamiento', "$ {:,}".format(500000)),
-    ('Punta del Este', 'Incluye aéreos y alojamiento', "$ {:,}".format(1000000))]
-    cursor.executemany("INSERT INTO paquetes (Nombre_paq, Descripcion, Precio) VALUES (?, ?, ?)", Lista_paquetes)
-    conexion.commit()
-    print("Datos insertados y confirmados.")
-
-#4. Consultar y mostrar todos los registros
-    print("\n---Paquetes---")
-    cursor.execute("SELECT Nombre_paq, Descripcion, Precio FROM paquetes")
-    registros= cursor.fetchall()
-    for fila in registros:
-       print(f"Nombre_paq: {fila[0]}, Descripcion: {fila[1]}, Precio: {fila[2]}")
-    print("Total de viajes: ", len(Lista_paquetes))
-
+    class Paquetes:
+        def __init__(self, ID, nombre_paq, descripcion, precio):
+            self.ID= ID
+            self.nombre_paq= nombre_paq
+            self.descripcion= descripcion
+            self.precio= precio
+            self.cant_ventas= int(input("Ingrese la cantidad de paquetes vendidos: "))
+            print(self.ID, "|", self.nombre_paq, "|", self.descripcion, "|", "$ {:,}".format(self.precio), "|", self.cant_ventas)
+            
+                    
+    def total_viajes():
+        viajes= paquete1.cant_ventas + paquete2.cant_ventas + paquete3.cant_ventas + paquete4. cant_ventas + paquete5.cant_ventas + paquete6.cant_ventas
+        print("Cantidad total de viajes: ", viajes)
+        
+    def total_ventas():
+        venta1= paquete1.cant_ventas * paquete1.precio
+        venta2= paquete2.cant_ventas * paquete2.precio
+        venta3= paquete3.cant_ventas * paquete3.precio
+        venta4= paquete4.cant_ventas * paquete4.precio
+        venta5= paquete5.cant_ventas * paquete5.precio
+        venta6= paquete6.cant_ventas * paquete6.precio
+        ventas= venta1 + venta2 + venta3 + venta4 + venta5 + venta6
+        print("\n---Total de ventas---")
+        print("Paquete 1: ", "$ {:,}".format(venta1))
+        print("Paquete 2: ", "$ {:,}".format(venta2))
+        print("Paquete 3: ", "$ {:,}".format(venta3))
+        print("Paquete 4: ", "$ {:,}".format(venta4))
+        print("Paquete 5: ", "$ {:,}".format(venta5))
+        print("Paquete 6: ", "$ {:,}".format(venta6))
+        print("Total ventas: ", "$ {:,}".format(ventas))
+        
+    paquete1= Paquetes(1,'Ushuaia + Calafate', 'Incluye: Aéreos, alojamiento, traslados y excursiones', 1000000)
+    paquete2= Paquetes(2, 'Salta', 'Incluye: Aéreos, alojamiento, traslados y excursiones', 800000)
+    paquete3= Paquetes(3, 'Mendoza', 'Incluye: Aéreos, alojamiento, traslados y excursiones', 600000)
+    paquete4= Paquetes(4, 'Rio de Janeiro', 'Incluye: Aéreos y alojamiento', 500000)
+    paquete5= Paquetes(5, 'Punta del Este', 'Incluye: Aéreos y alojamiento', 1000000)
+    paquete6= Paquetes(6, 'Lima + Cuzco', 'Incluye: Aéreos, alojamiento, traslados y excursiones', 2000000)
+    total_viajes() 
+    total_ventas()
+    
 except sqlite3.Error as e:
     print(f"Ocurrió un error con SQLite: {e}")
     
--- TP final DOO.py
+++ TP final DOO.py
@@ -2,10 +2,10 @@ import sqlite3
 import sys
 
 #1. Conexión a la base de datos
-def get_connection():
+def conectar():
     try:
         conexion= sqlite3.connect("pasajeros.sql")
-        conexion.row_factory = sqlite3.Row
+        conexion.row_factory= sqlite3.Row
         return conexion
     
     except sqlite3.Error as e:
@@ -14,7 +14,7 @@ def get_connection():
 
 #2. Creamos la tabla
 def crear_tabla():
-    conexion= get_connection()
+    conexion= conectar()
     cursor= conexion.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS pasajeros
     (DNI INTEGER PRIMARY KEY,
@@ -22,128 +22,131 @@ def crear_tabla():
     Fecha_nac TEXT NOT NULL,
     Telefono INTEGRER,
     Mail TEXT NOT NULL,
-    Paquete INTEGRER)''')    
+    Paquete INTEGRER FOREIGN KEY)''')    
     conexion.commit()
     conexion.close()
     print("Tabla 'pasajeros' verificada/creada.")
 
 #---Funciones ABM---
+#3. Alta de pasajero:
+def alta():
+    try:
+        DNI= int(input("Ingrese DNI del pasajero: "))
+    except ValueError:
+        print("Debe agregar un número de hasta 8 cifras.")
+        return
+    nombre= input("Ingrese nombre y apellido del pasajero: ")
+    fecha_nac= input("Ingrese fecha de nacimiento del pasajero: ")
+    try:
+        telefono= int(input("Ingrese telefono del pasajero: "))
+    except ValueError:
+        print("Debe agregar un número de 10 cifras.")
+        return
+    mail= input("Ingrese mail del pasajero: ")
+    try:
+        paquete= int(input("Ingrese código del paquete elegido por el pasajero: "))
+    except ValueError:
+        print("Debe agregar un número del 1 al 6.")
+        return
+    conexion= conectar()
+    cursor= conexion.cursor()
+    
+    try:
+        cursor.execute("INSERT INTO pasajeros (DNI, Nombre, Fecha_nac, Telefono, Mail, Paquete) VALUES (?, ?, ?, ?, ?, ?)",
+        (DNI, nombre, fecha_nac, telefono, mail, paquete))
+        conexion.commit()
+        print(f"Pasajero '{nombre}' agregado con éxito.")        
+    except sqlite3.Error as e:
+        print(f"Error al agregar pasajero: {e}")
+    finally:
+        conexion.close()
+
+#4. Lista de pasajeros
+def lista():
+    conexion= conectar()
+    cursor= conexion.cursor()
+    cursor.execute("SELECT * FROM pasajeros")
+    pasajeros= cursor.fetchall()
+    conexion.close()
+    if not pasajeros:
+        print("No hay pasajeros registrados.")
+        return
+    
+    print("\n---Lista de pasajeros ---")
+    for pax in pasajeros:
+        print(f"DNI: {pax['DNI']} | Nombre: {pax['nombre']} | Fecha de nacimiento: {pax['fecha_nac']} | Telefono: {pax['telefono']} | Mail: {pax['mail']} | Paquete: {pax['paquete']}")
+    print("---------------------------\n")
+
+#5. Modificación de datos    
+def modificar_telefono():
+    lista()
+    pax_id= input("Ingrese DNI del pasajero a modificar: ")
+    conexion= conectar()
+    cursor= conexion.cursor()
+    cursor.execute("SELECT * FROM pasajeros WHERE DNI= ?", (pax_id,))
+    pasajero_existente= cursor.fetchone()
+    if not pasajero_existente:
+        print(f"No se encontró un pasajero con DNI {pax_id}.")
+        conexion.close()
+        return
+    try:
+        nuevo_telefono= int(input(f"Ingrese el nuevo telefono de {pasajero_existente['nombre']} (actual: {pasajero_existente['telefono']}): "))
+    except ValueError:
+        print("Debe ser un número de 10 cifras.")
+        conexion.close()
+        return
+    try:
+        cursor.execute("UPDATE pasajeros SET Telefono= ? WHERE DNI= ?", (nuevo_telefono, pax_id))
+        conexion.commit()
+        print("telefono actualizado correctamente.")
+    except sqlite3.Error as e:
+        print(f"Error al modificar telefono: {e}")
+    finally:
+        conexion.close()
+    
+def modificar_mail():
+    lista()
+    pax_id= input("Ingrese DNI del pasajero a modificar: ")
+    conexion= conectar()
+    cursor= conexion.cursor()
+    cursor.execute("SELECT * FROM pasajeros WHERE DNI= ?", (pax_id,))
+    pasajero_existente= cursor.fetchone()
+    if not pasajero_existente:
+        print(f"No se encontró un pasajero con DNI {pax_id}.")
+        conexion.close()
+        return
+    nuevo_mail= (input(f"Ingrese el nuevo mail de {pasajero_existente['nombre']} (actual: {pasajero_existente['mail']}): "))
+    try:
+        cursor.execute("UPDATE pasajeros SET Mail= ? WHERE DNI= ?", (nuevo_mail, pax_id))
+        conexion.commit()
+        print("Mail actualizado correctamente.")
+    except sqlite3.Error as e:
+        print(f"Error al modificar mail: {e}")
+    finally:
+        conexion.close()    
+
+#6. Baja de pasajeros
+def baja():
+    lista()
+    pax_id = input("Ingrese el DNI del pasajero a eliminar: ")
+    conexion= conectar()
+    cursor= conexion.cursor()
+    try:
+        cursor.execute("DELETE FROM pasajeros WHERE DNI= ?", (pax_id,))
+        conexion.commit()
+        if cursor.rowcount > 0:
+            print(f"Pasajero con DNI {pax_id} eliminado correctamente.")
+        else:
+            print(f"No se encontró un pasajero con DNI {pax_id}.")
+    except sqlite3.Error as e:
+        print(f"Error al eliminar pasajero: {e}")
+    finally:
+        conexion.close()
+
 class Pasajeros:
     def __init__(self):
         pass
     
-    def alta(self):
-        try:
-            DNI= int(input("Ingrese DNI del pasajero: "))
-        except ValueError:
-            print("Debe agregar un número.")
-            return
-        nombre= input("Ingrese nombre y apellido del pasajero: ")
-        fecha_nac= input("Ingrese fecha de nacimiento del pasajero: ")
-        try:
-            telefono= int(input("Ingrese telefono del pasajero: "))
-        except ValueError:
-            print("Debe agregar un número.")
-            return
-        mail= input("Ingrese mail del pasajero: ")
-        try:
-            paquete= int(input("Ingrese código del paquete elegido por el pasajero: "))
-        except ValueError:
-            print("Debe agregar un número.")
-            return
-        conexion= get_connection()
-        cursor= conexion.cursor()
-    
-        try:
-            cursor.execute("INSERT INTO pasajeros (DNI, Nombre, Fecha_nac, Telefono, Mail, Paquete) VALUES (?, ?, ?, ?, ?, ?)",
-            (DNI, nombre, fecha_nac, telefono, mail, paquete))
-            conexion.commit()
-            print(f"Pasajero '{nombre}' agregado con éxito.")        
-        except sqlite3.Error as e:
-            print(f"Error al agregar pasajero: {e}")
-        finally:
-            conexion.close()
-
-    def lista(self):
-        conexion= get_connection()
-        cursor= conexion.cursor()
-        cursor.execute("SELECT * FROM pasajeros")
-        pasajeros= cursor.fetchall()
-        conexion.close()
-        if not pasajeros:
-            print("No hay pasajeros registrados.")
-            return
-    
-#Accediendo a los datos por nombre de columna gracias a row_factory
-        print("\n---Lista de pasajeros ---")
-        for pax in pasajeros:
-            print(f"DNI: {pax['DNI']} | Nombre: {pax['nombre']} | Fecha de nacimiento: {pax['fecha_nac']} | Telefono: {pax['telefono']} | Mail: {pax['mail']} | Paquete: {pax['paquete']}")
-        print("---------------------------\n")
-    
-    def modificar_telefono(self):
-        self.lista()
-        pax_id= input("Ingrese DNI del pasajero a modificar: ")
-        conexion= get_connection()
-        cursor= conexion.cursor()
-        cursor.execute("SELECT * FROM pasajeros WHERE DNI= ?", (pax_id,))
-        pasajero_existente= cursor.fetchone()
-        if not pasajero_existente:
-            print(f"No se encontró un pasajero con DNI {pax_id}.")
-            conexion.close()
-            return
-        try:
-            nuevo_telefono= int(input(f"Ingrese el nuevo telefono de {pasajero_existente['nombre']} (actual: {pasajero_existente['telefono']}): "))
-        except ValueError:
-            print("Debe ser un número.")
-            conexion.close()
-            return
-        try:
-            cursor.execute("UPDATE pasajeros SET Telefono= ? WHERE DNI= ?", (nuevo_telefono, pax_id))
-            conexion.commit()
-            print("telefono actualizado correctamente.")
-        except sqlite3.Error as e:
-            print(f"Error al modificar telefono: {e}")
-        finally:
-            conexion.close()
-            
-    def modificar_mail(self):
-        self.lista()
-        pax_id= input("Ingrese DNI del pasajero a modificar: ")
-        conexion= get_connection()
-        cursor= conexion.cursor()
-        cursor.execute("SELECT * FROM pasajeros WHERE DNI= ?", (pax_id,))
-        pasajero_existente= cursor.fetchone()
-        if not pasajero_existente:
-            print(f"No se encontró un pasajero con DNI {pax_id}.")
-            conexion.close()
-            return
-        nuevo_mail= (input(f"Ingrese el nuevo mail de {pasajero_existente['nombre']} (actual: {pasajero_existente['mail']}): "))
-        try:
-            cursor.execute("UPDATE pasajeros SET Mail= ? WHERE DNI= ?", (nuevo_mail, pax_id))
-            conexion.commit()
-            print("Mail actualizado correctamente.")
-        except sqlite3.Error as e:
-            print(f"Error al modificar Mail: {e}")
-        finally:
-            conexion.close()    
-        
-    def baja(self):
-        self.lista()
-        pax_id = input("Ingrese el DNI del pasajero a eliminar: ")
-        conexion= get_connection()
-        cursor= conexion.cursor()
-        try:
-            cursor.execute("DELETE FROM pasajeros WHERE DNI= ?", (pax_id,))
-            conexion.commit()
-            if cursor.rowcount > 0:
-                print(f"Pasajero con DNI {pax_id} eliminado correctamente.")
-            else:
-                print(f"No se encontró un pasajero con DNI {pax_id}.")
-        except sqlite3.Error as e:
-            print(f"Error al eliminar pasajero: {e}")
-        finally:
-            conexion.close()
-
 #--- Menú Principal ---
     def menu(self):
         while True:
@@ -156,9 +159,9 @@ class Pasajeros:
             
             opcion= input("Seleccione una opción: ")
             if opcion== '1':
-                self.alta()
+                alta()
             elif opcion== '2':
-                self.lista()
+                lista()
             elif opcion== '3':
                 while True:
                     print("¿Qué dato desea modificar?")
@@ -167,14 +170,14 @@ class Pasajeros:
                     
                     opcion2= input("Seleccione una opción: ")
                     if opcion2== 'a':
-                        self.modificar_telefono()
+                        modificar_telefono()
                     elif opcion2== 'b':
-                        self.modificar_mail()
+                        modificar_mail()
                         break
                     else:
                         print("Opción incorrecta.")
             elif opcion== '4':
-                self.baja()
+                baja()
             elif opcion== '5':
                 print("¡Gracias!")
                 break
