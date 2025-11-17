b171686de326b7e1ac6166c1f23567bc002f3e93
Daniela <daniela.comes@outlook.com>
2025-11-17 12:09:15 -0300

    Cambios 7

--- Pasajeros/TKinter.py
+++ Pasajeros/TKinter.py
@@ -1,6 +1,32 @@
 import tkinter as tk
 from tkinter import ttk
+import sqlite3
+import sys
 
+def conectar():
+    try:
+        conexion= sqlite3.connect("pasajeros.sql")
+        conexion.row_factory= sqlite3.Row
+        return conexion    
+    except sqlite3.Error as e:
+        print(f"Error al conectar con la base de datos: {e}")
+        sys.exit(1)
+        
+def crear_tabla():
+    conexion= conectar()
+    cursor= conexion.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS pasajeros
+    (DNI INTEGER PRIMARY KEY,
+    Nombre TEXT NOT NULL,
+    Fecha_nac TEXT NOT NULL,
+    Telefono INTEGRER,
+    Mail TEXT NOT NULL,
+    Paquete INTEGRER TEXT NOT NULL)''')
+    conexion.commit()
+    conexion.close() 
+    print("Tabla 'pasajeros' verificada/creada.")
+    
+    
 class Pasajero():
     def __init__(self, DNI, nombre, fecha_nac, telefono, mail, paquete):
         self.DNI= DNI
@@ -9,39 +35,45 @@ class Pasajero():
         self.telefono= telefono
         self.mail= mail
         self.paquete= paquete
-
-def alta(pasajero):
-    try:
-        DNI= int(input("Ingrese el DNI del pasajero: "))
-    except ValueError:
-        print("Debe ingresar un número de hasta 8 cifras.")
-        return
-    nombre= input("Ingrese nombre y apellido del pasajero: ")
-    fecha_nac= input("Ingrese fecha de nacimiento del pasajero: ")
-    try:
-        telefono= int(input("Ingrese el teléfono del pasajero: "))
-    except ValueError:
-        print("Debe ingresar un número de 10 cifras.")
-        return
-    mail= input("Ingrese el mail del pasajero: ")
-    try:
-        paquete= int(input("Ingrese el ID del paquete elegido por el pasajero: "))
-    except ValueError:
-        print("Debe ingresar un número del 1 al 6.")
-        return
+    
+def guardar(pasajero):
     conexion= conectar()
     cursor= conexion.cursor()
-
     try:
         cursor.execute("INSERT INTO pasajeros (DNI, Nombre, Fecha_nac, Telefono, Mail, Paquete) VALUES (?, ?, ?, ?, ?, ?)",
-        (DNI, nombre, fecha_nac, telefono, mail, paquete))
+        (pasajero.DNI, pasajero.nombre, pasajero.fecha_nac, pasajero.telefono,pasajero.mail, pasajero.paquete))
         conexion.commit()
-        print(f"Pasajero '{nombre}' agregado con éxito.")        
+        print(f"Pasajero '{nombre}' agregado con éxito.")           
     except sqlite3.Error as e:
-        print(f"Error al agregar empleado: {e}")
+        print(f"Error al agregar pasajero: {e}")
     finally:
         conexion.close()
+        
+def listar():
+    conexion= conectar()
+    cursor= conexion.cursor()
+    lista_pasajeros= []
+    try:
+        cursor.execute("SELECT * FROM pasajeros")
+        lista_pasajeros= cursor.fetchall()
+        conexion.close()
+    except:
+        print("La tabla no existe.")
+    return lista_pasajeros
 
+def modificar(pasajero, DNI):
+    conexion= conectar()
+    cursor= conexion.cursor()
+    sql= f"""UPDATE pasajeros SET Nombre= '{pasajero.nombre}', Fecha_nac= '{pasajero.fecha_nac}', Telefono= '{pasajero.telefono}', 
+        Mail= '{pasajero.mail}, Paquete= '{pasajero.paquete}' WHERE DNI= '{DNI}'"""
+    
+    try:
+        cursor.execute(sql)            
+        conexion.commit()
+        conexion.close()
+    except:
+        print("No se pudo editar el registro.")
+       
 class Aplicacion(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -51,7 +83,8 @@ class Aplicacion(tk.Tk):
         self.habilitar()
         self.deshabilitar()
         self.guardar()
-        self.tabla()
+        self.tabla_pasajeros()
+        self.editar()
 
     def datos(self):
         #Etiquetas        
@@ -91,14 +124,10 @@ class Aplicacion(tk.Tk):
         #Botones
         self.boton1= tk.Button(self, text= "Agregar pasajero", command= self.habilitar)
         self.boton1.grid(row= 6, column= 0)
-        self.boton2= tk.Button(self, text= "Modificar pasajero")
+        self.boton2= tk.Button(self, text= "Modificar pasajero", command= self.editar)
         self.boton2.grid(row= 6, column= 1)
         self.boton3= tk.Button(self, text= "Eliminar pasajero")
         self.boton3.grid(row= 6, column= 2)
-        self.boton4= tk.Button(self, text= "Guardar", command= self.guardar)
-        self.boton4.grid(row= 8, column= 0)
-        self.boton5= tk.Button(self, text= "Cancelar", command= self.deshabilitar)
-        self.boton5.grid(row= 8, column= 1)
         self.boton6= tk.Button(self, text= "Salir")
         self.boton6.grid(row= 8, column= 2)
         
@@ -110,8 +139,11 @@ class Aplicacion(tk.Tk):
         self.texto5.config(state= 'normal')
         self.texto6.config(state= 'normal')
         
-        self.boton4.config(state= 'disabled')
-        self.boton5.config(state= 'disabled')
+        self.boton1.config(state= 'normal')
+        self.boton2.config(state= 'normal')
+        self.boton3.config(state= 'normal')
+        self.boton4.config(state= 'normal')
+        self.boton5.config(state= 'normal')
         
     def deshabilitar(self):
         self.dni.set('')
@@ -139,12 +171,15 @@ class Aplicacion(tk.Tk):
             self.telefono.get(),
             self.mail.get(),
             self.paquete.get(),)
-        alta(pasajero)
+        guardar(pasajero)
+        self.tabla_pasajeros()
         self.deshabilitar()
 
-    def tabla(self):
+    def tabla_pasajeros(self):
+        self.lista_pasajeros= listar()
+        self.lista_pasajeros.reverse()
         self.tabla= ttk.Treeview(self, column= ('DNI', 'Nombre', 'Fecha de nacimiento', 'Telefono', 'Mail', 'Paquete'))
-        self.tabla.grid(row= 7, column= 0, columnspan= 5, padx= 10, pady= 10)
+        self.tabla.grid(row= 7, column= 0, columnspan= 6, padx= 10, pady= 10)
         self.tabla.heading('#0', text= 'DNI')
         self.tabla.heading('#1', text= 'Nombre')
         self.tabla.heading('#2', text= 'Fecha de nacimiento')
@@ -152,8 +187,28 @@ class Aplicacion(tk.Tk):
         self.tabla.heading('#4', text= 'Mail')
         self.tabla.heading('#5', text= 'Paquete')
         
-        self.tabla.insert('', 0, text= 33741053,
-        values= ('Daniela Comes', '20/06/1988', 1156290262, 'daniela.comes@outlook.com', 1))
+        for p in self.lista_pasajeros:
+            self.tabla.insert('', 0, text= p[0],
+            values= (p[1], p[2], p[3], p[4], p[5]))
+            
+    def editar(self):
+        try:
+            self.dni= self.tabla.item(self.tabla_pasajeros.selection())['text']
+            self.nombre= self.tabla.item(self.tabla_pasajeros.selection())['values'][1]
+            self.fecha= self.tabla.item(self.tabla_pasajeros.selection())['values'][2]
+            self.telefono= self.tabla.item(self.tabla_pasajeros.selection())['values'][3]
+            self.mail= self.tabla.item(self.tabla_pasajeros.selection())['values'][4]
+            self.paquete= self.tabla.item(self.tabla_pasajeros.selection())['values'][5]
+            self.habilitar()
+            
+            self.texto1.insert(0, self.dni)
+            self.texto2.insert(1, self.nombre)
+            self.texto3.insert(2, self.fecha)
+            self.texto4.insert(3, self.telefono)
+            self.texto5.insert(4, self.mail)
+            self.texto6.insert(5, self.paquete)
+        except:
+            print("No ha seleccionado ningún registro.")
     
 if __name__ == "__main__":
     app= Aplicacion()
